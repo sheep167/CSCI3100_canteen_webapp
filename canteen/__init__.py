28e7e6ae550038e11a1e8d37fbca28079089a538
Marc Yiu <yiuchunto@gmail.com>
2022-02-27 22:50:22 +0800

    Implemented the email verification using SendGrid SMTP

--- canteen/__init__.py
+++ canteen/__init__.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from flask_pymongo import PyMongo
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/canteen'
 app.config['SECRET_KEY'] = 'dQw4w9WgXcQ'
 mongo = PyMongo(app)
 login_manager = LoginManager(app)
+
+app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'apikey'
+app.config['MAIL_PASSWORD'] = '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'
+app.config['MAIL_DEFAULT_SENDER'] = 'yiuchunto@gmail.com'
+mail = Mail(app)
 
 from canteen.routes import *
--- canteen/routes.py
+++ canteen/routes.py
@@ -1,14 +1,21 @@
-from canteen import app, mongo
+from canteen import app, mongo, mail
 from flask import render_template, redirect, url_for, flash
 from flask_login import login_user, logout_user
 from .form import UserRegistrationForm, UserLoginForm
 from .models import User, LoginUser
 import bcrypt
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired
+from flask_mail import Message
 
 
 @app.route('/', methods=['GET'])
 def home_page():
     return render_template('home_page.html')
+
+
+def generate_confirmation_token(email):
+    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    return serializer.dumps(email, salt=app.config['SECRET_KEY'])
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -22,6 +29,12 @@ def register_page():
         user_to_create = User(email=form.email.data,
                               username=form.username.data,
                               password=hashed_password)
+
+        token = generate_confirmation_token(form.email.data)
+
+        msg = Message('Verification Email', recipients=[form.email.data])
+        msg.body = 'Go here to for verification. http://localhost:5000/confirm_email/%s' % token
+        mail.send(msg)
 
         mongo.db.users.insert_one(user_to_create.to_json())
         return redirect(url_for('login_page'))
@@ -39,9 +52,12 @@ def login_page():
     if form.validate_on_submit():
         attempted_user = mongo.db.users.find_one({'email': form.email.data})
         if attempted_user and bcrypt.checkpw(form.password.data.encode('utf-8'), attempted_user.get('password')):
-            login_user(LoginUser(attempted_user))
-            flash('Successfully Logged In as: %s' % attempted_user.get('username'), category='success')
-            return redirect(url_for('home_page'))
+            if attempted_user.get('confirmed') == 1:
+                login_user(LoginUser(attempted_user))
+                flash('Successfully Logged In as: %s' % attempted_user.get('username'), category='success')
+                return redirect(url_for('home_page'))
+            else:
+                flash('Not authenticated', category='danger')
         else:
             flash('Email and Password do not match', category='danger')
 
@@ -54,3 +70,22 @@ def logout_page():
     flash('You have been logged out!', category='info')
     return redirect(url_for('home_page'))
 
+
+@app.route('/confirm_email/<token>')
+def confirm_email(token):
+    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    try:
+        email = serializer.loads(token, salt=app.config['SECRET_KEY'], max_age=300)
+        mongo.db.users.update_one({'email': email}, {'$set': {'confirmed': 1}})
+    except SignatureExpired:
+        return '<h1> Token Expired </h1> '
+    return redirect(url_for('home_page'))
+
+
+# @app.route('/test')
+# def test():
+#     msg = Message('Twilio SendGrid Test Email', recipients=['yiuchunto@gmail.com'])
+#     msg.body = 'This is a test email!'
+#     msg.html = '<p>This is a test email!</p>'
+#     mail.send(msg)
+#     return 'ok'
