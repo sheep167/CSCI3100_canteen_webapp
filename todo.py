892ab330a1c573cf8ed53d7ddf873f1605ba492a
Marc Yiu <yiuchunto@gmail.com>
2022-03-03 04:02:04 +0800

    Added todo.py, for putting any features needed in the future

---
+++ todo.py
@@ -0,0 +1,7 @@
+# TODO: 1. Active link in navbar when switching tabs https://stackoverflow.com/questions/18600031/changing-the-active-class-of-a-link-with-the-twitter-bootstrap-css-in-python-fla
+# TODO: 2. Admin Page - Add Dishes button in Canteen tab, with upload image function. /add/canteen/<_id>/dishes /edit/canteen/<_id>/dishes /delete/canteen/<_id>/dishes
+# TODO: 3. Admin Page - Add Comment button in Canteen tab. /add/canteen/<_id>/comment /edit/canteen/<_id>/comment /delete/canteen/<_id>/comment
+# TODO: 4. Admin Page - Add Order button in Canteen tab. /add/canteen/<_id>/order /edit/canteen/<_id>/order /delete/canteen/<_id>/order
+# TODO: 5. User POV - All of it.
+# TODO: 6. Shopping Cart Function (?) https://roytuts.com/simple-shopping-cart-using-python-flask-mysql/
+# TODO: 7. data.py - Add more data for initialisation
