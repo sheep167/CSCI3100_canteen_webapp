eeaf8e45af733a8ef0233ed58e247f5b5d27cbc1
Marc Yiu <yiuchunto@gmail.com>
2022-03-03 04:13:52 +0800

    Added todo.py, for putting any features needed in the future

--- todo.py
+++ todo.py
@@ -1,7 +1,8 @@
 # TODO: 1. Active link in navbar when switching tabs https://stackoverflow.com/questions/18600031/changing-the-active-class-of-a-link-with-the-twitter-bootstrap-css-in-python-fla
-# TODO: 2. Admin Page - Add Dishes button in Canteen tab, with upload image function. /add/canteen/<_id>/dishes /edit/canteen/<_id>/dishes /delete/canteen/<_id>/dishes
-# TODO: 3. Admin Page - Add Comment button in Canteen tab. /add/canteen/<_id>/comment /edit/canteen/<_id>/comment /delete/canteen/<_id>/comment
-# TODO: 4. Admin Page - Add Order button in Canteen tab. /add/canteen/<_id>/order /edit/canteen/<_id>/order /delete/canteen/<_id>/order
+# TODO: 2. Admin Page - Add Dishes button in Canteen tab, with upload image function. /add/canteen/<_id>/dishes /edit/canteen/<_id>/dishes/<dishes_id> /delete/canteen/<_id>/dishes/<dishes_id>
+# TODO: 2.1 ObjectId in json text field is shown as the name of the canteen. Later convert to _id in the backend
+# TODO: 3. Admin Page - Add Comment button in Canteen tab. /add/canteen/<canteen_id>/comment /edit/canteen/<canteen_id>/comment/<comment_id> /delete/canteen/<canteen_id>/comment/<comment_id>
+# TODO: 4. Admin Page - Add Order button in Canteen tab. /add/canteen/<canteen_id>/order /edit/canteen/<canteen_id>/order/<order_id> /delete/canteen/<canteen_id>/order/<order_id>
 # TODO: 5. User POV - All of it.
 # TODO: 6. Shopping Cart Function (?) https://roytuts.com/simple-shopping-cart-using-python-flask-mysql/
 # TODO: 7. data.py - Add more data for initialisation
