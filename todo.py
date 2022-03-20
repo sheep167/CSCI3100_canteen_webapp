a819941c58dc6112631953dadfa874e957f8da08
Marc Yiu <yiuchunto@gmail.com>
2022-03-20 19:11:07 +0800

    1 task done in todo

--- todo.py
+++ todo.py
@@ -1,5 +1,5 @@
 backend
-# TODO: 1. Active link in navbar when switching tabs https://stackoverflow.com/questions/18600031/changing-the-active-class-of-a-link-with-the-twitter-bootstrap-css-in-python-fla
+# DONE: TODO: 1. Active link in navbar when switching tabs https://stackoverflow.com/questions/18600031/changing-the-active-class-of-a-link-with-the-twitter-bootstrap-css-in-python-fla
 # TODO: 2. Admin Page - Add Dishes button in Canteen tab, with upload image function. /add/canteen/<_id>/dishes /edit/canteen/<_id>/dishes/<dishes_id> /delete/canteen/<_id>/dishes/<dishes_id>
 # TODO: 2.1 ObjectId in json text field is shown as the name of the canteen. Later convert to _id in the backend
 # TODO: 3. Admin Page - Add Comment button in Canteen tab. /add/canteen/<canteen_id>/comment /edit/canteen/<canteen_id>/comment/<comment_id> /delete/canteen/<canteen_id>/comment/<comment_id>
