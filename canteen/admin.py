ba8bc2e304e8aa496e4c60b4de89855f100045c2
Marc Yiu <yiuchunto@gmail.com>
2022-03-24 00:59:55 +0800

    Add and delete order for admin user. Edit is not supported since it is too complicated.

--- canteen/admin.py
+++ canteen/admin.py
@@ -3,7 +3,7 @@ import os
 from canteen import app, mongo
 from flask import render_template, request, redirect, flash
 from flask_login import current_user, login_required
-from .form import DataEditForm, DataEditFormWithImage
+from .form import DataEditForm, DataEditFormWithImage, DataEditFormWithSelect
 from werkzeug.utils import secure_filename
 import json
 from json import JSONDecodeError
@@ -50,6 +50,8 @@ def add_data_page(category):
             form.text.data = json.dumps({'email': 'str', 'password': 'str', 'username': 'str', 'auth_type': 'int', 'confirmed': 'int', 'balance': 'float'}, indent=4)
         elif category == 'canteens':
             form.text.data = json.dumps({'name': 'str', 'longitude': 'float', 'latitude': 'float', 'open_at': 'str', 'close_at': 'str', 'capacity': 'int'}, indent=4)
+        else:
+            return 'Not Found', 404
 
     if request.method == 'POST':
         try:
@@ -99,8 +101,7 @@ def edit_data_page(category, _id):
 
         except JSONDecodeError:
             flash('Cannot decode JSON. Please check and try again.', category='error')
-        except ValidationError:
-            flash('Duplicate keys with the database', category='error')
+
     return render_template('data.html', form=form, method='Edit', category=category)
 
 
@@ -162,6 +163,31 @@ def overview_canteens_dishes(canteen_id, category):
         comments = list(results)
         return render_template('admin_comments.html', canteen=canteen, comments=comments)
 
+    elif category == 'orders':
+        canteen = mongo.db.canteens.find_one({'_id': ObjectId(canteen_id)})
+
+        results = mongo.db.orders.aggregate([
+            {'$match': {'at_canteen': ObjectId(canteen_id)}},
+            {'$lookup':
+                {'from': 'users',
+                 'localField': 'by_user',
+                 'foreignField': '_id',
+                 'as': 'by_user'}},
+            {'$lookup':
+                {'from': 'dishes',
+                 'localField': 'dishes',
+                 'foreignField': '_id',
+                 'as': 'dishes'}},
+            {'$set': {'by_user': {'$arrayElemAt': ['$by_user', 0]}}},
+            {'$set': {'by_user': '$by_user.username'}},
+            {'$set': {'dishes': '$dishes.name'}}
+        ])
+
+        orders = list(results)
+        for order in orders:
+            order['dishes'] = ', '.join(order.get('dishes'))
+        return render_template('admin_orders.html', canteen=canteen, orders=orders)
+
 
 @app.route('/add/canteens/<canteen_id>/<category>', methods=['GET', 'POST'])
 @login_required
@@ -179,6 +205,8 @@ def add_canteens_data(canteen_id, category):
 
     if category == 'dishes':
         form = DataEditFormWithImage()
+    elif category == 'orders':
+        form = DataEditFormWithSelect()
     else:
         form = DataEditForm()
 
@@ -187,6 +215,23 @@ def add_canteens_data(canteen_id, category):
             form.text.data = json.dumps({'name': 'str', 'price': 'float', 'ingredients': 'List[str]', 'rating': 'int/null'}, indent=4)
         elif category == 'comments':
             form.text.data = json.dumps({'at_time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'username': 'str', 'rating': 'int', 'paragraph': 'str'}, indent=4)
+        elif category == 'orders':
+            form.text.data = json.dumps({'at_time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'username': 'str', 'order_status': 'waiting, fulfilled, unfulfilled'}, indent=4)
+
+            dishes = mongo.db.dishes.aggregate([
+                {'$match': {'at_canteen': ObjectId(canteen_id)}},
+                {'$lookup':
+                    {'from': 'canteens',
+                     'localField': 'at_canteen',
+                     'foreignField': '_id',
+                     'as': 'at_canteen'}},
+                {'$set': {'at_canteen': {'$arrayElemAt': ['$at_canteen', 0]}}},
+                {'$set': {'at_canteen': '$at_canteen.name'}},
+                {'$project': {'name': 1, '_id': 0}}
+            ])
+            dishes = [dish.get('name') for dish in dishes]
+
+            form.select.choices = dishes
 
     if request.method == 'POST':
         try:
@@ -211,7 +256,33 @@ def add_canteens_data(canteen_id, category):
 
                 user = mongo.db.users.find_one({'username': data.get('username')})
                 if not user:
-                    raise NoSuchUserError
+                    raise NoSuchUserError()
+                user_id = user.get('_id')
+                del data['username']
+                data['by_user'] = ObjectId(user_id)
+
+                data['at_time'] = datetime.datetime.strptime(data.get('at_time'), '%Y-%m-%d %H:%M:%S')
+
+            elif category == 'orders':
+
+                dishes_ids = mongo.db.dishes.aggregate([
+                    {'$match': {'name': {'$in': form.select.data}}},
+                    {'$project': {'_id': 1}}
+                ])
+                data['dishes'] = [_id.get('_id') for _id in dishes_ids]
+
+                total_price = mongo.db.dishes.aggregate([
+                    {'$match': {'name': {'$in': form.select.data}}},
+                    {'$group': {'_id': 0, 'total_price': {'$sum': '$price'}}}
+                ])
+                total_price = list(total_price)[0].get('total_price')
+                data['total_price'] = total_price
+
+                data['at_canteen'] = ObjectId(canteen_id)
+
+                user = mongo.db.users.find_one({'username': data.get('username')})
+                if not user:
+                    raise NoSuchUserError()
                 user_id = user.get('_id')
                 del data['username']
                 data['by_user'] = ObjectId(user_id)
@@ -230,7 +301,11 @@ def add_canteens_data(canteen_id, category):
         except ValueError:
             flash('Wrong time format', category='error')
 
-    return render_template('data_with_image.html', form=form, method='Add')
+    if category == 'dishes':
+        return render_template('data_with_image.html', form=form, method='Edit')
+    if category == 'orders':
+        return render_template('data_with_select.html', form=form, method='Edit')
+    return render_template('data.html', form=form, method='Edit')
 
 
 @app.route('/edit/canteens/<canteen_id>/<category>/<_id>', methods=['GET', 'POST'])
@@ -261,6 +336,8 @@ def edit_canteens_data(canteen_id, category, _id):
             form.text.data = json.dumps(mongo.db.dishes.find_one({'_id': ObjectId(_id)}, {'_id': 0, 'image_path': 0, 'at_canteen': 0}), indent=4, default=str)
         elif category == 'comments':
             form.text.data = json.dumps(mongo.db.comments.find_one({'_id': ObjectId(_id)}, {'_id': 0, 'at_canteen': 0, 'by_user': 0, 'at_time': 0}), indent=4, default=str)
+        # Editing the orders is not supported as this action is complicated
+        # in both database query and editing them on screen
 
     if request.method == 'POST':
         try:
