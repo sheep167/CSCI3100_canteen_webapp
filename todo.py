cf466c3c5df0c797d2a6a5cc98a491490533cfee
Marc Yiu <yiuchunto@gmail.com>
2022-04-03 02:58:11 +0800

    added button to open canteen location in google map

--- todo.py
+++ todo.py
@@ -5,5 +5,4 @@ back-end
 # TODO: 1. User POV - All of it.
 # TODO: 2. data.py - Add more data for initialisation
 # TODO: 3. cast type in admin form
-# TODO: 4. (href) Open Google Map to show canteen location
 # TODO: 5. Canteen Manager to see the order and change the order status
