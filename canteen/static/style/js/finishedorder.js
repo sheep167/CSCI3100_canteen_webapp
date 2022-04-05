423e523804ecaf02661d3d567d0959f762282c11
Maneemala2013 <s6007503@mwit.ac.th>
2022-04-06 00:39:55 +0700

    Edit canteen order page

--- canteen/static/style/js/finishedorder.js
+++ canteen/static/style/js/finishedorder.js
@@ -1,11 +1,20 @@
-function yesorno(){
+function yesorno() {
     document.getElementById('finish').innerHTML = '';
     document.getElementById('yes').innerHTML = '<button type="button" class="btn btn-success" display="inline-block" onclick="notFinish()">Yes</button>';
     document.getElementById('no').innerHTML = '<button type="button" class="btn btn-danger" display="inline-block" onclick="notFinish()">No</button>';
 }
-  
-function notFinish(){
+
+function notFinish() {
     document.getElementById('finish').innerHTML = '<button type="button" class="btn btn-primary" onclick="yesorno()">Finish</button>';
     document.getElementById('yes').innerHTML = ''
     document.getElementById('no').innerHTML = ''
+}
+
+function confirmFinish() {
+    let text = "Have you finished this order?";
+    if (confirm(text) == true) {
+        //
+    } else {
+        //
+    }
 }
