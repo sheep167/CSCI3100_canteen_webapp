cd61e70a3504a3389d65c0394da3f6cf9d7005fc
Marc Yiu <yiuchunto@gmail.com>
2022-05-02 17:29:19 +0800

    comments added

--- canteen/form.py
+++ canteen/form.py
@@ -4,8 +4,9 @@ from wtforms.validators import Length, Email, DataRequired, ValidationError
 from canteen import mongo
 
 
+# Form used for user registration
 class UserRegistrationForm(FlaskForm):
-
+    # Validate functions are named as validate_variable
     def validate_email(self, email_to_check):
         user = mongo.db.users.find_one({'email': email_to_check.data})
         if user:
@@ -16,27 +17,37 @@ class UserRegistrationForm(FlaskForm):
         if user:
             raise ValidationError('Username has been taken')
 
+    # Specify the input field
+    # Use pre-made validators
     email = StringField(label='Enter Email:', validators=[Email(), DataRequired()])
     username = StringField(label='Enter Username:', validators=[Length(min=2, max=20), DataRequired()])
     password = PasswordField(label='Enter Password:', validators=[Length(min=6), DataRequired()])
     submit = SubmitField(label='Register Account')
 
 
+# Form used to handle user login
 class UserLoginForm(FlaskForm):
     email = StringField(label='Enter Email:', validators=[Email(), DataRequired()])
     password = PasswordField(label='Enter Password:', validators=[Length(min=6), DataRequired()])
     submit = SubmitField(label='Sign in')
 
 
+# Form used for admin to create and edit data
+# No validators because of flexibility.
+# This form is used for many categories
 class DataEditForm(FlaskForm):
     text = TextAreaField()
     submit = SubmitField()
 
+
+# Form used for admin with image selection
 class DataEditFormWithImage(FlaskForm):
     text = TextAreaField()
     image = FileField()
     submit = SubmitField()
 
+
+# Form used for admin with dropdown menu selection
 class DataEditFormWithSelect(FlaskForm):
     text = TextAreaField()
     select = SelectMultipleField()
